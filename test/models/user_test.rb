f5bbba4d2fcafe64b6f17df3ebb4e2d8b9244400
Haplo <haplodragon@gmail.com>
2017-10-27 23:09:37 -0500

    Complete status feed of followed users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -102,4 +102,25 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
   end
 
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+
+    # Posts from followed user should be included
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+
+    # Posts from self should be included
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+
+    # Posts from unfollowed user should NOT be included
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
